6e93bbab4a7264e5aadf78a7cdc16eaaeb3f8d19
Froxick <nvorozhevv@mail.ru>
2026-01-16 00:30:49 +0500

    Init Project

---
+++ shared/constants/theme.ts
@@ -0,0 +1,24 @@
+import { ColorsType } from "../types/ColorsType";
+
+
+export const Colors : {
+  light : ColorsType,
+  dark : ColorsType
+} = {
+  light: {
+    text: '',
+    background: '',
+    content: '',
+    icon: '',
+    tabIconDefault: '',
+    tabIconSelected: '',
+  },
+  dark: {
+    text: '',
+    background: '',
+    content: '',
+    icon: '',
+    tabIconDefault: '',
+    tabIconSelected: '',
+  },
+};
---
+++ shared/types/ColorsType.ts
@@ -0,0 +1,8 @@
+export interface ColorsType {
+        text: string,
+        background: string,
+        content: string,
+        icon: string,
+        tabIconDefault: string,
+        tabIconSelected: string,
+}
