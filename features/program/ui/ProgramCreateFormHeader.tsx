2159811224816051e38c30d43915d64f54a85e3e
Froxick <nvorozhevv@mail.ru>
2026-02-18 23:29:30 +0500

    ProgramCreateForm Info block

--- features/program/ui/ProgramCreateFormHeader.tsx
+++ features/program/ui/ProgramCreateFormHeader.tsx
@@ -5,6 +5,9 @@ interface ProgramCreateFormHeaderProps {
     step: number,
 }
 export const ProgramCreateFormHeader = ({step} : ProgramCreateFormHeaderProps) => {
+    const headerInfoText = [
+        'Укажите название программы','Придумайте названия неделям','Укажите дни отдыха','Настройте первую неделю'
+    ]
     const styles = StyleSheet.create({
         container: {
             justifyContent: 'center',
@@ -21,10 +24,17 @@ export const ProgramCreateFormHeader = ({step} : ProgramCreateFormHeaderProps) =
             fontWeight: 'bold'
         },
         infoBlock: {
-
+            padding: 15,
+            backgroundColor: '#1c1212',
+            borderRadius: 15,
+            marginTop: 10,
+            borderWidth: 1,
+            borderColor: Colors.border
         },
         infoBlockText: {
-
+            color: Colors.darkTextSecondary,
+            fontWeight: 'bold',
+            fontSize: 15
         }
     })
     return (
@@ -48,7 +58,7 @@ export const ProgramCreateFormHeader = ({step} : ProgramCreateFormHeaderProps) =
             />
             <View style={styles.infoBlock}>
                 <Text style={styles.infoBlockText}>
-                    
+                    {headerInfoText[step - 1]}
                 </Text>
             </View>
         </View>
